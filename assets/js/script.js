cf7e3c7cc09326aa44fddeb57c680556d8e03261
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-03-06 13:58:47 +0000

    Add popup with rules when hover over heading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-let gameType = document.getElementsByClassName('gameChoice');
+// Get the game choice made by the player
+let gameChoice = document.getElementsByClassName('gameChoice');
+
+//Event trigger - if player moves over the image on the game heading
+let catOne = document.getElementById('imgId');
+
+catOne.addEventListener("mouseover", mouseOverImage);
+catOne.addEventListener("mouseout", mouseOutImage);
+
+function mouseOverImage() {
+    console.log('mouse over');
+    document.getElementById("gR").style.display = "block";
+}
+
+function mouseOutImage() {
+    console.log('mouse out');
+    document.getElementById("gR").style.display = "";
+}   
