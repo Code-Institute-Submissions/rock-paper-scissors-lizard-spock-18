14988a855bc937900513f79abd156a4f0accb51c
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-03-06 17:44:30 +0000

    Add event listener to catch when play presses button and then changes players choice image to match.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,50 @@
 // Get the game choice made by the player
 let gameChoice = document.getElementsByClassName('gameChoice');
 
-//Event trigger - if player moves over the image on the game heading
-let catOne = document.getElementById('imgId');
+/**
+ * Only once the page has loaded run the game
+ */
+document.addEventListener("DOMContentLoaded",runGame());
 
-catOne.addEventListener("mouseover", mouseOverImage);
-catOne.addEventListener("mouseout", mouseOutImage);
+function runGame() {
+    /**
+     * The player can view the game rules, as stated by Sheldon,
+     * when the hover over the game heading.
+     */
+    let imageId = document.getElementById('imgId');
+
+    imageId.addEventListener("mouseover", mouseOverImage);
+    imageId.addEventListener("mouseout", mouseOutImage);
+
+    /**
+     * An event listener to catch when a player click on a button
+     */
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("id") === "rockButton") {
+                document.getElementById("p").setAttribute('style','background: url("assets/images/rock-1.png") no-repeat center center; background-size: 115% 115%;');
+            } else if (this.getAttribute("id") === "paperButton") {
+                document.getElementById("p").setAttribute('style','background: url("assets/images/paper-1.png") no-repeat center center; background-size: 115% 115%;');
+            } else if (this.getAttribute("id") === "scissorsButton") {
+                document.getElementById("p").setAttribute('style','background: url("assets/images/scissors-1.png") no-repeat center center; background-size: 115% 115%;');
+            } else if (this.getAttribute("id") === "lizardButton") {
+                document.getElementById("p").setAttribute('style','background: url("assets/images/lizard-1.png") no-repeat center center; background-size: 115% 115%;');
+            } else if (this.getAttribute("id") === "spockButton") {
+                document.getElementById("p").setAttribute('style','background: url("assets/images/spock-1.png") no-repeat center center; background-size: 115% 115%;');
+            } 
+        })
+    }
+
+}
+
 
 function mouseOverImage() {
-    console.log('mouse over');
     document.getElementById("gR").style.display = "block";
 }
 
 function mouseOutImage() {
-    console.log('mouse out');
     document.getElementById("gR").style.display = "";
 }   
+
