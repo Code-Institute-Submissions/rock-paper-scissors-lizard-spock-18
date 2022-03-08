8b34e03eb515915cd49f52e47a96511f0446d6a8
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-03-08 21:44:04 +0000

    Add playGame and resetGame functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,35 +6,49 @@ document.addEventListener("DOMContentLoaded", gameType());
 /**
  * The gameType function
  * Disables the game board and runs the getChoice function
- * once the player chooses an option the runGame is called and the game board is enabled
+ * to let player choose the game type
  */
 function gameType() {
     // Get the game choice made by the player
     document.getElementById('imgId').setAttribute("style", "opacity: 0.5; pointer-events: none;");
     document.getElementById('gameOff').setAttribute("style", "opacity: 0.5; pointer-events: none;");
-    gameChoiceId = document.getElementById("gameChoiceId");
-    gameChoiceId.addEventListener("click", getChoice);
-    runGame();
+    gameChoiceId = document.getElementById('gameChoiceId');
+    gameChoiceId.addEventListener('click', getChoice);
 }
 
 /**
  * The getChoice function
  * Disables the game board and catches the game type radio button the player clicks on.
- * once the player chosses an option the game runs and the game board is enabled
+ * once the player chooses an option the game runs and the game board is enabled
  */
 function getChoice() {
     let gameChoice = document.getElementsByClassName('gameChoice');
-
     for (let radio of gameChoice) {
         if (radio.checked === true) {
             document.getElementById('imgId').setAttribute("style", "opacity: 1; pointer-events: auto;");
             document.getElementById('gameOff').setAttribute("style", "opacity: 1; pointer-events: auto;");
-            document.getElementById('computerScore').innerText = "0";
-            document.getElementById('playerScore').innerText = "0";
+            // document.getElementById('computerScore').innerText = "0";
+            // document.getElementById('playerScore').innerText = "0";
          }
+    }
+    if (document.getElementsByTagName('input')[0].checked) {
+        runGame('3');
+    } else if (document.getElementsByTagName('input')[1].checked) {
+        runGame('5');
+    } else if (document.getElementsByTagName('input')[2].checked) {
+        runGame('0');
     }
  }
 
+
+ /**
+  * The runGame function
+  * the logic will receive the players choice when clicking on a button
+  * then running the computerPlay function.
+  * Once bot players have played, the gameLogic function is call to decide on a winner
+  * followed by incrementScore function to increment the winner's score
+  * @param {*} gameChosen parameter is the number of turn the player will have before end game
+  */
  function runGame(gameChosen) {
 
     /**
@@ -46,41 +60,92 @@ function getChoice() {
     imageId.addEventListener("mouseover", mouseOverImage);
     imageId.addEventListener("mouseout", mouseOutImage);
 
-    /**
-     * An event listener to catch when a player clicks on a button.
-     * The choice made by the player will then change the image on Players Choice.
-     * The next step is to call the computer to play
-     */
     let buttons = document.getElementsByTagName("button");
+    
+    console.log(buttons.length);
+    console.log(gameChosen);
+    for (let button of buttons) {
+        let counter = 0;
+        // console.log("Infor");
+        button.addEventListener("click", playGame(button, gameChosen));
+        button.removeEventListener("click", playGame());
+        counter++;
+        console.log("counter++");
+        console.log(counter);
+
+        incrementScore();
+
+        if (counter > gameChosen) {
+        let computerScore = parseInt(document.getElementById('computerScore').innerText);
+        let playerScore = parseInt(document.getElementById('playerScore').innerText);
+        let text;
+        
+        if (computerScore === playerScore) {
+            text = "It's a draw.";
+            alert('Game Over. ' + text);
+        } else if (computerScore > playerScore) {
+            text = "Computer wins.";
+            alert('Game Over. ' + text);
+        } else if (computerScore < playerScore) {
+            text = "Player wins.";
+            alert('Game Over. ' + text);
+        }
+
+        counter = resetGame();
+        console.log("counter reset");
+        console.log(counter);
+        }
+    }
+}
+
+function resetGame() {
+   document.getElementById('computerScore').innerText = "0";
+   document.getElementById('playerScore').innerText = "0";
+   document.getElementById('midDiv').innerText = "Play Again?";
+   document.getElementById('imgId').setAttribute("style", "opacity: 0.5; pointer-events: none;");
+   document.getElementById('gameOff').setAttribute("style", "opacity: 0.5; pointer-events: none;");
+   document.getElementsByTagName('input')[0].checked = false;
+   document.getElementsByTagName('input')[1].checked = false;
+   document.getElementsByTagName('input')[2].checked = false;
+   return 0;
+}
+
+/**
+* The playGame function
+* Has an event listener to catch when a player clicks on a button.
+* The choice made by the player will then change the image on Players Choice.
+* The next step is to call the computer to play. Once both plays are made
+* the gameLogic function is called to work out who won. Then the incrementScore
+* function increments the winners score
+*/
+function playGame(button, gameChosen) {
     let playerChoice;
     let computerChoice;
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("id") === "rockButton") {
-                document.getElementById("p").setAttribute('style','background: url("assets/images/rock-1.png") no-repeat center center; background-size: 115% 115%;');
-                playerChoice = this.getAttribute("data-choice");
-                computerChoice = computerPlay();
-            } else if (this.getAttribute("id") === "paperButton") {
-                document.getElementById("p").setAttribute('style','background: url("assets/images/paper-1.png") no-repeat center center; background-size: 115% 115%;');
-                playerChoice = this.getAttribute("data-choice");
-                computerChoice = computerPlay();
-            } else if (this.getAttribute("id") === "scissorsButton") {
-                document.getElementById("p").setAttribute('style','background: url("assets/images/scissors-1.png") no-repeat center center; background-size: 115% 115%;');
-                playerChoice = this.getAttribute("data-choice");
-                computerChoice = computerPlay();
-            } else if (this.getAttribute("id") === "lizardButton") {
-                document.getElementById("p").setAttribute('style','background: url("assets/images/lizard-1.png") no-repeat center center; background-size: 115% 115%;');
-                playerChoice = this.getAttribute("data-choice");
-                computerChoice = computerPlay();
-            } else if (this.getAttribute("id") === "spockButton") {
-                document.getElementById("p").setAttribute('style','background: url("assets/images/spock-1.png") no-repeat center center; background-size: 115% 115%;');
-                playerChoice = this.getAttribute("data-choice");
-                computerChoice = computerPlay();
-            }
-            playGame(playerChoice, computerChoice);
-            incrementScore();
-        })
+    // let counter=0;
+
+    console.log("ButtonEventListner");
+    if (button.getAttribute("id") === "rockButton") {
+        document.getElementById("playerChoiceId").setAttribute('style','background: url("assets/images/rock-1.png") no-repeat center center; background-size: 115% 115%;');
+        playerChoice = button.getAttribute("data-choice");
+        computerChoice = computerPlay();
+    } else if (button.getAttribute("id") === "paperButton") {
+        document.getElementById("playerChoiceId").setAttribute('style','background: url("assets/images/paper-1.png") no-repeat center center; background-size: 115% 115%;');
+        playerChoice = button.getAttribute("data-choice");
+        computerChoice = computerPlay();
+    } else if (button.getAttribute("id") === "scissorsButton") {
+        document.getElementById("playerChoiceId").setAttribute('style','background: url("assets/images/scissors-1.png") no-repeat center center; background-size: 115% 115%;');
+        playerChoice = button.getAttribute("data-choice");
+        computerChoice = computerPlay();
+    } else if (button.getAttribute("id") === "lizardButton") {
+        document.getElementById("playerChoiceId").setAttribute('style','background: url("assets/images/lizard-1.png") no-repeat center center; background-size: 115% 115%;');
+        playerChoice = button.getAttribute("data-choice");
+        computerChoice = computerPlay();
+    } else if (button.getAttribute("id") === "spockButton") {
+        document.getElementById("playerChoiceId").setAttribute('style','background: url("assets/images/spock-1.png") no-repeat center center; background-size: 115% 115%;');
+        playerChoice = button.getAttribute("data-choice");
+        computerChoice = computerPlay();
     }
+    gameLogic(playerChoice, computerChoice);
 }
 
 /**
@@ -89,11 +154,11 @@ function getChoice() {
  * the rules, as stated by sheldon, are displayed
  */
 function mouseOverImage() {
-    document.getElementById("gR").style.display = "block";
+    document.getElementById("gameRulesId").style.display = "block";
 }
 
 function mouseOutImage() {
-    document.getElementById("gR").style.display = "";
+    document.getElementById("gameRulesId").style.display = "";
 }   
 
 /**
@@ -107,30 +172,30 @@ function computerPlay() {
     let computerChoice;
 
     if (num === 0) {
-        document.getElementById("c").setAttribute('style','background: url("assets/images/rock-1.png") no-repeat center center; background-size: 115% 115%;');
+        document.getElementById("computerChoiceId").setAttribute('style','background: url("assets/images/rock-1.png") no-repeat center center; background-size: 115% 115%;');
         computerChoice = "rock";
     } else if (num === 1) {
-        document.getElementById("c").setAttribute('style','background: url("assets/images/paper-1.png") no-repeat center center; background-size: 115% 115%;');
+        document.getElementById("computerChoiceId").setAttribute('style','background: url("assets/images/paper-1.png") no-repeat center center; background-size: 115% 115%;');
         computerChoice = "paper";
     } else if (num === 2) {
-        document.getElementById("c").setAttribute('style','background: url("assets/images/scissors-1.png") no-repeat center center; background-size: 115% 115%;');
+        document.getElementById("computerChoiceId").setAttribute('style','background: url("assets/images/scissors-1.png") no-repeat center center; background-size: 115% 115%;');
         computerChoice = "scissors";
     } else if (num === 3) {
-        document.getElementById("c").setAttribute('style','background: url("assets/images/lizard-1.png") no-repeat center center; background-size: 115% 115%;');
+        document.getElementById("computerChoiceId").setAttribute('style','background: url("assets/images/lizard-1.png") no-repeat center center; background-size: 115% 115%;');
         computerChoice = "lizard";
     } else if (num === 4) {
-        document.getElementById("c").setAttribute('style','background: url("assets/images/spock-1.png") no-repeat center center; background-size: 115% 115%;');
+        document.getElementById("computerChoiceId").setAttribute('style','background: url("assets/images/spock-1.png") no-repeat center center; background-size: 115% 115%;');
         computerChoice = "spock";
     }
     return computerChoice;
 }
 
 /**
- * The playGame function
+ * The gameLogic function
  * This is where the rules of the game, as stated by sheldon,
  * are played out in a conditional IF statement
 */
-function playGame(playerChoice, computerChoice) {
+function gameLogic(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         document.getElementById("midDiv").innerText = "It's a draw";
     //Scissors and Paper
@@ -192,14 +257,17 @@ function playGame(playerChoice, computerChoice) {
  */
 function incrementScore() {
     let whoWon = document.getElementById("midDiv").innerText;
-    let playerScore = parseInt(document.getElementById("playerScore").innerText);
-    let computerScore = parseInt(document.getElementById("computerScore").innerText);
-
+    let playerScores = parseInt(document.getElementById("playerScore").innerText);
+    let computerScores = parseInt(document.getElementById("computerScore").innerText);
+    
     if (whoWon === "Player wins") {
-        playerScore += 1;
-        document.getElementById("playerScore").innerText = playerScore;
+        playerScores += 1;
+        document.getElementById("playerScore").innerText = playerScores.toString();
     } else if (whoWon === "Computer wins") {
-        computerScore += 1;
-        document.getElementById("computerScore").innerText = computerScore;
+        computerScores += 1;
+        document.getElementById("computerScore").innerText = computerScores.toString();
     }
+    console.log('increment');
+    console.log(computerScores);
+    console.log(playerScores);
 }
